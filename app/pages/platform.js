fe57917882c547e679f5368ed56f135a3dfc4396
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2024-10-28 18:46:42 +0530

    production grade?

--- app/pages/platform.js
+++
@@ -1,9 +0,0 @@
-export default function Platform() {
-    return (
-      <div className="flex flex-col min-h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold">Welcome to the Platform!</h1>
-        <p className="mt-4">Here you can explore all the features.</p>
-      </div>
-    );
-  }
-  
